a4c67e13ec16375a3b839182929ac319f7f9b76a
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 14:35:17 -0700

    7-prints_teb

---
+++ 0x01-variables_if_else_while/7-prints_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - a script that writes the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char L;
+
+	for (L = 'z' ; L <= 'a' ; L--)
+	{
+		putchar(L);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
