30c0474b23d50633a56662a807bb10ddda72f713
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 09:03:17 -0700

    fixed errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char* error_message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char* error_message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, error_message, strlen(error_message));
 	return (1);
 }
