ab55c5ed35d4ba90b87a939e46f6964fb357e044
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 16:43:42 -0700

    102-prints_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - all possible combinations of two two-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i ; j < 9; j++)
+		{
+			for (k = i ; k < 10; k++)
+			{
+				for (l = j+1; l <10 ; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+
+					if (i != 9 || j != 8 || k != 9 || l != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
