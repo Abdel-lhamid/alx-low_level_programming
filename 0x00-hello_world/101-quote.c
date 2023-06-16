b6763fc47d6b9ae10381fe9a122b96b64a6b1d71
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 09:01:57 -0700

    fix 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
-
+#include <string.h>
 /**
  * main - writes the required message to the standard error
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	char* error_message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(STDERR_FILENO, error_message, strlen(error_message));
 	return (1);
 }
