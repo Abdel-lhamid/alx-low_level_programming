8e4c4767aabc19735c1887784691db9f3fbacc1f
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 13:47:08 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - a script that writes the alphabets
+ * Return: 0
+ */
+
+int main(void) {
+	char L;
+
+	for (L = 'a' ; L <= 'z' ; L++)
+	{
+		putchar(L);
+	}
+	putchar("\n");
+	
+	return (0);
+}
