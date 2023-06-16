d786d49225526d3cdb909cc346e5ad000d85120a
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 16:57:30 -0700

    fix num 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,13 +10,14 @@ int main(void)
 	int j;
 	int k;
 	int l;
+
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i ; j < 9; j++)
+		for (j = 0 ; j < 9; j++)
 		{
 			for (k = i ; k < 10; k++)
 			{
-				for (l = j+1; l <10 ; l++)
+				for (l = j + 1; l < 10 ; l++)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
