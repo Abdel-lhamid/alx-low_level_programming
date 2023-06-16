738f4f8bd3c50fc5390ed4db98f303ff45d8ee6b
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 14:09:16 -0700

    4-prints_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - a script that writes the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char L;
+
+	for (L = 'a' ; L <= 'z' ; L++)
+	{
+		if (l != 'q' || l != 'e')
+		{
+			putchar(L);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
