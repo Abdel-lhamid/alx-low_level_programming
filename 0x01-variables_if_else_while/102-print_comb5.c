86c993a6a889f97afea2da04d5d771f2100751a6
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-16 17:11:59 -0700

    fix another solution

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,29 +8,21 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 98; i++)
 	{
-		for (j = 0 ; j < 9; j++)
+		for (j = i + 1 ; j < 99; j++)
 		{
-			for (k = i ; k < 10; k++)
-			{
-				for (l = j + 1; l < 10 ; l++)
-				{
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar(' ');
-					putchar('0' + k);
-					putchar('0' + l);
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
 
-					if (i != 9 || j != 8 || k != 9 || l != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
