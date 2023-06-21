4f1baf27d66775fdbf49f157f565efe4904e3ad9
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-20 18:49:15 -0700

    11 print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers to 98
+ * @n: the number
+ *
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
