306b44a10e847be9708759784994bd09151dc2e0
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-20 18:31:00 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that returns adds
+ * @a: first number
+ * @b: second number
+ * Return: returns the add value
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
