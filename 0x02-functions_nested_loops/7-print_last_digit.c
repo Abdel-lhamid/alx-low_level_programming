167618c3868d8ffe3d16bff84265267656e60523
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-20 17:21:30 -0700

    7 last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit
+ * @n the number we want to extract the last digit
+ * Return: the last digit
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+	
+	return (ld);
+}
