199f9dd22263ea199e790a8949d009757c1cb56f
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-21 16:56:30 -0700

    fix 100

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,11 +11,11 @@ int main(void)
 	long f2 = 2;
 	long f3;
 
-	printf("%d, %d", f1, f2);
+	printf("%li, %li", f1, f2);
 	for (i = 3; i <= 50; i++)
 	{
 		f3 = f1 + f2;
-		printf(", %d", f3);
+		printf(", %li", f3);
 		f1 = f2;
 		f2 = f3;
 	}
