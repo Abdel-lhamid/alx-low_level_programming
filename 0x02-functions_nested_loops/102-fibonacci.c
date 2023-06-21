db31b2445c4c2de0490e39f31843574ab3862cff
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-21 12:49:31 -0700

    102 fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main : list the first 5à fibonaccci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int f1, f2, f3;
+
+	printf("%d, %d", f1, f2);
+	for (i = 0; i <=50; i++)
+	{
+		f3 = f1 + f2;
+		printf(", %d", f3);
+		f1 = f2;
+		f2 = f3;
+	}
+	printf("\n");
+
+	return (0);
+}
