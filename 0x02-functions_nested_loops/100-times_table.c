218a15c4a8d4abd9e818373c653a67f3df809f74
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-21 11:58:32 -0700

    fix 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 	int j;
 	int r;
 
-	if (n > 0 && n < 15)
+	if (n => 0 && n < 15)
 	{
 		for (i = 0; i <= n ; i++)
 		{
@@ -24,18 +24,24 @@ void print_times_table(int n)
 				}
 				else if (r < 10)
 				{
-					_putchar(','); _putchar(' '); _putchar(' '); _putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar('0' + r);
 				}
 				else if (r < 100)
 				{
-					_putchar(','); _putchar(' '); _putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar('0' + (r / 10));
 					_putchar('0' + (r % 10));
 				}
 				else
 				{
-					_putchar(','); _putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 					_putchar('0' + (r / 100));
 					_putchar('0' + ((r / 10) % 10));
 					_putchar('0' + (r % 10));
