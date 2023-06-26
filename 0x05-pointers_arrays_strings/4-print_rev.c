72363f430fe7604075fc78279ec3d937cf25dd40
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-26 09:27:21 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print the revers of a string
+ * @s: the string we want to reverse
+ *
+ */
+
+void print_rev(char *s)
+{
+	_puts( strrev(s));
+}
