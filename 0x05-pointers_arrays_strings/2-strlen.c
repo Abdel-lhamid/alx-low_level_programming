12cb835ca4bc0012d6b1d51fffa293e971afb570
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-26 09:03:26 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the size of string
+ * @s: the string we want to calculate the lenght for
+ * Return: the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(*s);
+	return (len);
+}
