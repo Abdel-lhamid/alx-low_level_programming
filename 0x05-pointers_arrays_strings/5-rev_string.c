71851bc24235d752d6b3454cd4316ead0f5880f0
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-26 10:52:34 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2strlen.c"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string we want to reverse
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i,j;
+	int len = _strlen(s);
+	char temp;
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
