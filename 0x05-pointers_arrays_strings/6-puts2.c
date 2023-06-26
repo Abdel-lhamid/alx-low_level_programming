4297d5e249589ac2e476f05f7cdb9a2f6604299e
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-26 11:22:50 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of the string
+ * @str: the string we gonna use
+ */
+void puts2(char *str)
+{
+	int len = _strlen(str);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+		i++
+	}
+}
