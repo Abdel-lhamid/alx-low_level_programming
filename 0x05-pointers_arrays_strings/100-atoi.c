3e2fcbe2f8ed248d6e02b5d559e3a7d29885b015
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-28 04:41:49 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the int we got from the string
+ */
+int _atoi(char *s)
+{
+	int dig;
+	int si = 1;
+	int result= 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-' || *s == '+')
+		{
+			if (*s == '-')
+				si = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			di = *s - '0';
+			result = result * 10 + (si * dig);
+		}
+		else if (result != '0')
+		{
+			break;
+		}
+		s++;
+	}
+	return (result);
+}
