f8b6da9e76a8c2ac9778ee072dd15457f2ee68d3
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-30 17:45:45 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "mail.h"
+
+/**
+ * cap_string - capitalize word of a string
+ * @s: the string to work with
+ * Return: the string with words capitalized
+ */
+
+char *cap_string(char *)
+{
+	int i;
+
+	if(s[0] >= 'a' && s[i] <= 'z')
+		s[0] = s[0] - 32;
+
+	for (i = 0; s[i] != 'a'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') 
+				&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+				s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+				s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+				s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+				|| s[i - 1] == '\n'))
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
