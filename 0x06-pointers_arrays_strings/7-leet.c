ac49d1df937fce56d9aaafc2d6e7e2d6eda52bec
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-06-30 18:36:48 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * @leet - encodes a string into 1337
+ * @s: the string to manipulate
+ *
+ * Return: the encoded strring
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char *ltrs = "aAeEoOtTlL";
+	char *leets = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; ltrs [j] != '\0'; j++)
+		{
+			if (s[i] == ltrs[j])
+			{
+				s[i] = leets[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
