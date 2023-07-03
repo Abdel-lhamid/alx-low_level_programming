3852f684390d73cb3ff248cee8d633fd2669c53b
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-03 08:40:48 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #define NULL 0
 
 /**
- * _strchr - search for a specefic 
+ * _strchr - search for a specefic
  * @s: the table to search
  * @c: the char to search for
  * Return: the pointer to the c char otherwise null
@@ -10,16 +10,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while ( *s != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
-			return (s);
+			return(s);
 			break;
 		}
 		s++;
 	}
-	
+
 	if (*s == c)
 		return (s);
 	else
