5a1ddeb1ff6ad09694fa60fc24967f35efc49c10
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-05 10:57:26 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s: the string we want to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		putchar('\n');
+		return;
+	}
+	else
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
