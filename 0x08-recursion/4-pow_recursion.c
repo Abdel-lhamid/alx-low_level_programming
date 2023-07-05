e991d414031bfd01aa8aa9cb6ba6953564775be9
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-05 11:44:32 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0 || x == 1)
+		return (1);
+	else if (x == 0)
+		return (0);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
