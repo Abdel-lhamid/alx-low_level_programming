8760a87bea697821f392593bab039ff205613604
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-07 12:23:38 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+		ptintf("%s\n",argv[i]);
+	return (0);
+}
