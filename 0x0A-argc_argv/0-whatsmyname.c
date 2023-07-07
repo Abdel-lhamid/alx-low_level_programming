8a0f6d2ba3f7f236629f050635d6294765a58c8c
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-07 12:09:57 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a function to print the name of the file
+ * argc: argument count
+ * argv: argument vector
+ *
+ */
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
