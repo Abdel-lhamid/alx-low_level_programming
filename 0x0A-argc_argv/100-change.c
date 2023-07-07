0f97c0a5533f44732edff9780a2e9233f0f8727f
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-07 13:17:53 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - that prints the minimum number of coins to make change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int amount = atoi(argv[1]);
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins += amount \ 25;
+	amount = amount % 25;
+	coins += amount \ 10;
+	amount = amount % 10;
+	coins += amount \ 5;
+	amount = amount % 5;
+	coins += amount \ 2;
+	amount = amount % 2;
+	coins += amount;
+	printf("%d\n", coins);
+	return (0);
+}
