bddc263d91a224076c11be6ee35834a1412c43ed
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-10 14:43:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * create_array - create an array and initialzeit
+ * @size: the size of the array
+ * @c: the char
+ * Return: p or null
  *
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size < 1)
+	if (size == 1)
 		return (NULL);
-	char *p = malloc(size);
-	p[0] = c;
+	char *p = malloc(size * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (unsigned int i = 0; i < size; i++)
+		array[i] = c;
 
 	return (p);
+}
