751799e4eaacb0ae7d3273964ab0b4439a6daa6c
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-14 14:43:46 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-#include "0-malloc_checked.c"
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: size of the malloc
+ * Return: the pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
 
 /**
  * string_nconcat - function that concatenates two strings
