2921b7e9979b562172d379f5a057f4c68f697167
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-17 13:36:19 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: first
+ * @age: second
+ * @owner: third
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
