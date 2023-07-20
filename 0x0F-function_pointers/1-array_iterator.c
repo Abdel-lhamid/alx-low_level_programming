a44755964c38ae5586c516d133b937a63b2d158a
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-19 17:18:14 -0700

    1-array_iterator.c added stddef for size-t type

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * array_iterator - function that executes a functionon each element
  * @array: the array
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0;
+	size_i i = 0;
 
 	if (array && action && size > 0)
 	{
