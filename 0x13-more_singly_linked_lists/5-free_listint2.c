e6486f5ff0dc7db2c4c9af03eaef13c714e20264
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-08-01 15:59:48 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+}
