47bce09f3f51cdd29537a2a62a4a750086b954c4
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-08-07 18:55:43 -0700

    2-append_text_to_file.c removed extra )

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len]; len++)
 		;
 
-	n_wrote = write(fd, text_content, len));
+	n_wrote = write(fd, text_content, len);
 	if (n_wrote == -1 || n_wrote != len)
 	{
 		close(fd);
