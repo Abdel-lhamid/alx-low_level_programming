14846c48430543ea8715f2efcfada243c477a4fa
Abdel Hamid <abdelhamidbusiness@gmail.com>
2023-08-31 06:25:00 -0700

    betty style

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,8 +11,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
-			return (-1);
+		return (-1);
 
 	mask = 1UL << index;
 	*n = *n | mask;
