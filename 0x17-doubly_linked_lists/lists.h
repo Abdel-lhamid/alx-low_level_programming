bca5ff7a647c0bc841b3158afdb902739829767e
Abdel Hamid <abdelhamidbusiness@gmail.com>
2023-10-13 13:17:40 -0700

    tasks till 5

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - Doubly linked list node structure
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ *
+ * Description: This structure is used to represent a node in a doubly
+ * linked list.
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes */
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+#endif /* LIST_H */
+
