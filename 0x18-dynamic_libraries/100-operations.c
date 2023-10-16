7ae630aa01ef0071f19dcd5577830b6be40e8aeb
Abdel Hamid <abdelhamidbusiness@gmail.com>
2023-10-16 14:04:35 -0700

    fix funcs name

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -4,15 +4,15 @@ int add(int a, int b) {
     return a + b;
 }
 
-int subtract(int a, int b) {
+int sub(int a, int b) {
     return a - b;
 }
 
-int multiply(int a, int b) {
+int mul(int a, int b) {
     return a * b;
 }
 
-int divide(int a, int b) {
+int div(int a, int b) {
     if (b == 0) {
         printf("Error: Division by zero\n");
         return 0;  // You can choose to handle this error differently
@@ -20,7 +20,7 @@ int divide(int a, int b) {
     return a / b;
 }
 
-int modulus(int a, int b) {
+int mod(int a, int b) {
     if (b == 0) {
         printf("Error: Modulus by zero\n");
         return 0;  // You can choose to handle this error differently
