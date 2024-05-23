df8c71fe04f8f42a524a63df8fb883e1ed9c4f07
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-05-23 11:39:12 +0100

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -23,6 +23,7 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", l, array[l]);
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", l, h);
+	h = h < size ? h : size - 1;
 
 	while (l <= h && l < size)
 	{
